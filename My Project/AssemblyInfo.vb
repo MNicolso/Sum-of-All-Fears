cad94552f209a7a8019d4fdeaa4c7fea80b7f9e0
MNicolso <71354582+MNicolso@users.noreply.github.com>
2020-10-08 21:22:49 -0700

    Add project files.

---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+<Assembly: AssemblyTitle("Fear_of_All_Sums")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Fear_of_All_Sums")>
+<Assembly: AssemblyCopyright("Copyright ©  2020")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("dac8f861-53a5-45f7-8701-c4410b297900")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
---
+++ Scripts/script.js
@@ -0,0 +1,36 @@
+﻿document.getElementById("button").addEventListener('click', () => {
+    let minInput = parseInt(document.getElementById("minInput").value);
+    let maxInput = parseInt(document.getElementById("maxInput").value);
+    let fizzInput = parseInt(document.getElementById("fizzInput").value);
+    let buzzInput = parseInt(document.getElementById("buzzInput").value);
+
+    let arrayModified = modifiedData(minInput, maxInput, fizzInput, buzzInput);
+
+    document.getElementById('printBox').innerHTML = arrayModified;
+})
+
+//document.getElementsByTagName("input").addEventListener("keydown", function (e) {
+//    var character=(e.which)
+//}
+
+function modifiedData(minInput, maxInput, fizzInput, buzzInput) {
+    var arrayModified = "";
+    for (let i = minInput; i<= maxInput; i++) {
+        let fizzRemainder = i % fizzInput; 
+        let buzzRemainder = i % buzzInput; 
+        
+        if (fizzRemainder == 0 && buzzRemainder == 0) {
+            arrayModified += "FIZZBUZZ! ";
+        } else if (fizzRemainder == 0) {
+            arrayModified += "fizz ";
+        } else if (buzzRemainder == 0) {
+            arrayModified += "buzz ";
+        } else {
+            arrayModified += i+' ';
+        }
+
+    }
+
+
+    return arrayModified;
+}
